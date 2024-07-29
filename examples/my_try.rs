fa2a3321b89b95130aaff8b7461133e54dfdb382
maya <770257551@qq.com>
2024-07-29 09:43:25 +0800

    声明宏

--- examples/my_try.rs
+++ examples/my_try.rs
@@ -1,6 +1,4 @@
-
 use anyhow::{anyhow, Result};
-use macros::my_try;
 
 fn main() -> Result<()> {
     // let ret = f3(f2(f1("hello")?)?)?;
@@ -19,4 +17,14 @@ fn f2(s: impl AsRef<str>) -> Result<String> {
 
 fn f3(s: impl AsRef<str>) -> Result<String> {
     Err(anyhow!("f3: {}", s.as_ref()))
+}
+
+#[macro_export]
+macro_rules! my_try {
+    ($expr:expr) => {
+        match $expr {
+            Ok(val) => val,
+            Err(err) => return Err(err.into()),
+        }
+    };
 }
--- examples/my_vec.rs
+++ examples/my_vec.rs
@@ -1,6 +1,24 @@
-use macros::my_vec;
-
 fn main() {
-    let v = my_vec![1,2,3,];
+    let v = my_vec![1, 2, 3,];
     println!("{:?}", v)
+}
+
+#[macro_export]
+macro_rules! my_vec {
+    () => {
+        Vec::new()
+    };
+    ($elem:expr; $n:expr) => {
+        std::vec::from_elem($elem, $n)
+    };
+    ($($x:expr),+ $(,)?) => {
+        {
+            // let mut temp_vec = Vec::new();
+            // $(
+            //     temp_vec.push($x);
+            // )*
+            // temp_vec
+            <[_]>::into_vec(Box::new([$($x),*]))
+        }
+    };
 }
